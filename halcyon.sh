a8b73860d1fcd84f04b7d1a7f4222fad6430671a
Miëtek Bak <mietek@bak.io>
2014-10-16 06:42:49 +0100

    Update packaging

--- halcyon.sh
+++ halcyon.sh
@@ -1,12 +1,12 @@
 declare HALCYON_TOP_DIR
 HALCYON_TOP_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P )
 
-if ! [ -d "${HALCYON_TOP_DIR}/../bashmenot" ]; then
-	echo '   *** ERROR: Locating bashmenot failed' >&2
+if ! [ -d "${HALCYON_TOP_DIR}/lib/bashmenot" ]; then
+	echo '   *** ERROR: Cannot source bashmenot' >&2
 	exit 1
 fi
 
-source "${HALCYON_TOP_DIR}/../bashmenot/bashmenot.sh"
+source "${HALCYON_TOP_DIR}/lib/bashmenot/bashmenot.sh"
 source "${HALCYON_TOP_DIR}/src/cache.sh"
 source "${HALCYON_TOP_DIR}/src/storage.sh"
 source "${HALCYON_TOP_DIR}/src/constraints.sh"
