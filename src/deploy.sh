539fb2aa857059d33a367ec572502ae3e1c6f196
Miëtek Bak <mietek@bak.io>
2014-10-26 21:06:42 +0000

    Fix option ordering

--- src/deploy.sh
+++ src/deploy.sh
@@ -5,7 +5,7 @@ function detect_app_package () {
 
 	local package_file
 	package_file=$(
-		find "${source_dir}" -type f -maxdepth 1 -name '*.cabal' |
+		find "${source_dir}" -maxdepth 1 -type f -name '*.cabal' |
 		match_exactly_one
 	) || return 1
 
