e6757a59e5f24f161e0ba1ca7ed27d6a76488d97
Miëtek Bak <mietek@bak.io>
2014-10-26 21:34:58 +0000

    Tweak triggering hooks

--- src/slug.sh
+++ src/slug.sh
@@ -56,7 +56,7 @@ function prepare_slug () {
 
 	if [ -f "${source_dir}/.halcyon-magic/slug-extra-hook" ]; then
 		log 'Running slug extra hook'
-		if ! ( "${source_dir}/.halcyon-magic/slug-extra-hook" "${tag}" "${source_dir}" "${slug_dir}" |& quote ); then
+		if ! ( HALCYON_RECURSIVE=1 HALCYON_SLUG_DIR="${slug_dir}" "${source_dir}/.halcyon-magic/slug-extra-hook" "${tag}" "${source_dir}" "${slug_dir}" |& quote ); then
 			die 'Failed to run slug extra hook'
 		fi
 	fi
