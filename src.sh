0bce930c26dd6a2bafdf0269eb13248c018c6e50
Miëtek Bak <mietek@bak.io>
2014-11-02 18:31:47 +0000

    Add auto-updating

--- src.sh
+++ src.sh
@@ -1,44 +1,102 @@
+set -o pipefail
+
 export HALCYON_TOP_DIR
 HALCYON_TOP_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P )
 
-install_bashmenot () {
-	local dir
-	dir="${HALCYON_TOP_DIR}/lib/bashmenot"
-	if [[ -d "${dir}" ]]; then
-		if ! git -C "${dir}" fetch -q || ! git -C "${dir}" reset -q --hard '@{u}'; then
-			echo '   *** ERROR: Cannot update bashmenot' >&2
-			return 1
-		fi
+
+quote () {
+	sed 's/^/       /' >&2 || return 0
+}
+
+
+halcyon_source_bashmenot () {
+	if [[ -d "${HALCYON_TOP_DIR}/lib/bashmenot" ]]; then
+		BASHMENOT_NO_AUTOUPDATE="${HALCYON_NO_AUTOUPDATE:-0}" \
+			source "${HALCYON_TOP_DIR}/lib/bashmenot/src.sh" || return 1
 		return 0
 	fi
 
 	local urloid url branch
-	urloid="${HALCYON_BASHMENOT_SOURCE_URL:-https://github.com/mietek/bashmenot.git}"
+	urloid="${BASHMENOT_URL:-https://github.com/mietek/bashmenot.git}"
 	url="${urloid%#*}"
 	branch="${urloid#*#}"
-	if ! git clone -q "${url}" "${dir}"; then
-		echo "   *** ERROR: Cannot clone ${url}" >&2
-		return 1
+	if [[ "${branch}" == "${url}" ]]; then
+		branch='master'
 	fi
-	if [[ "${url}" != "${branch}" ]] && ! git -C "${dir}" checkout -q "${branch}"; then
-		echo "   *** ERROR: Cannot checkout bashmenot ${branch}" >&2
-		return 1
-	fi
+
+	echo '-----> Installing bashmenot' >&2
+
+	git clone "${url}" "${HALCYON_TOP_DIR}/lib/bashmenot" |& quote || return 1
+	git -C "${HALCYON_TOP_DIR}/lib/bashmenot" checkout "${branch}" |& quote || return 1
+
+	BASHMENOT_NO_AUTOUPDATE=1 \
+		source "${HALCYON_TOP_DIR}/lib/bashmenot/src.sh" || return 1
 }
 
-install_bashmenot || exit 1
 
-source "${HALCYON_TOP_DIR}/lib/bashmenot/src.sh"
+if ! halcyon_source_bashmenot; then
+	echo '   *** ERROR: Cannot source bashmenot' >&2
+fi
 
-source "${HALCYON_TOP_DIR}/src/app.sh"
-source "${HALCYON_TOP_DIR}/src/cabal.sh"
-source "${HALCYON_TOP_DIR}/src/constraints.sh"
-source "${HALCYON_TOP_DIR}/src/deploy.sh"
-source "${HALCYON_TOP_DIR}/src/ghc.sh"
-source "${HALCYON_TOP_DIR}/src/help.sh"
+
 source "${HALCYON_TOP_DIR}/src/paths.sh"
-source "${HALCYON_TOP_DIR}/src/sandbox.sh"
-source "${HALCYON_TOP_DIR}/src/slug.sh"
+source "${HALCYON_TOP_DIR}/src/vars.sh"
+source "${HALCYON_TOP_DIR}/src/deploy.sh"
 source "${HALCYON_TOP_DIR}/src/storage.sh"
 source "${HALCYON_TOP_DIR}/src/tag.sh"
-source "${HALCYON_TOP_DIR}/src/vars.sh"
+source "${HALCYON_TOP_DIR}/src/ghc.sh"
+source "${HALCYON_TOP_DIR}/src/cabal.sh"
+source "${HALCYON_TOP_DIR}/src/constraints.sh"
+source "${HALCYON_TOP_DIR}/src/app.sh"
+source "${HALCYON_TOP_DIR}/src/sandbox.sh"
+source "${HALCYON_TOP_DIR}/src/slug.sh"
+source "${HALCYON_TOP_DIR}/src/help.sh"
+
+
+halcyon_autoupdate () {
+	if (( ${HALCYON_NO_AUTOUPDATE:-0} )); then
+		return 0
+	fi
+
+	if [[ ! -d "${HALCYON_TOP_DIR}/.git" ]]; then
+		return 1
+	fi
+
+	local urloid url branch
+	urloid="${HALCYON_URL:-https://github.com/mietek/halcyon.git}"
+	url="${urloid%#*}"
+	branch="${urloid#*#}"
+	if [[ "${branch}" == "${url}" ]]; then
+		branch='master'
+	fi
+
+	log 'Auto-updating Halcyon'
+
+	local git_url must_update
+	must_update=0
+	git_url=$( git -C "${HALCYON_TOP_DIR}" ls-remote --get-url 'origin' ) || return 1
+	if [[ "${git_url}" != "${url}" ]]; then
+		git -C "${HALCYON_TOP_DIR}" remote set-url 'origin' "${url}" |& quote || return 1
+		must_update=1
+	fi
+
+	if ! (( must_update )); then
+		local mark_time current_time
+		mark_time=$( get_modification_time "${HALCYON_TOP_DIR}" ) || return 1
+		current_time=$( date +'%s' ) || return 1
+		if (( mark_time > current_time - 60 )); then
+			return 0
+		fi
+	fi
+
+	git -C "${HALCYON_TOP_DIR}" fetch 'origin' |& quote || return 1
+	git -C "${HALCYON_TOP_DIR}" reset --hard "origin/${branch}" |& quote || return 1
+
+	HALCYON_NO_AUTOUPDATE=1 \
+		source "${HALCYON_TOP_DIR}/src.sh" || return 1
+}
+
+
+if ! halcyon_autoupdate; then
+	log_warning 'Cannot auto-update Halcyon'
+fi
