691e27d51bda648d5ff0740e7f6bc601230dcb06
Miëtek Bak <mietek@bak.io>
2014-11-02 16:28:18 +0000

    Fix URL order

--- src/storage.sh
+++ src/storage.sh
@@ -311,11 +311,11 @@ install_pigz () {
 	local original_url
 	case "${os}" in
 	'linux-ubuntu-14.04-x86_64')
-		original_url='http://mirrors.kernel.org/ubuntu/pool/universe/p/pigz/pigz_2.1.5-1_amd64.deb';;
+		original_url='http://mirrors.kernel.org/ubuntu/pool/universe/p/pigz/pigz_2.3-2_amd64.deb';;
 	'linux-ubuntu-12.04-x86_64')
 		original_url='http://mirrors.kernel.org/ubuntu/pool/universe/p/pigz/pigz_2.1.6-1_amd64.deb';;
 	'linux-ubuntu-10.04-x86_64')
-		original_url='http://mirrors.kernel.org/ubuntu/pool/universe/p/pigz/pigz_2.3-2_amd64.deb';;
+		original_url='http://mirrors.kernel.org/ubuntu/pool/universe/p/pigz/pigz_2.1.5-1_amd64.deb';;
 	*)
 		die "Unexpected OS: ${description}"
 	esac
