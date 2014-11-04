c114b71e13ed0e54da4cbbde60f1f679b6a1fc7e
Miëtek Bak <mietek@bak.io>
2014-11-04 03:11:20 +0000

    Improve paths handling

--- src/paths.sh
+++ src/paths.sh
@@ -1,15 +1,20 @@
-export HALCYON_DIR="${HALCYON_DIR:-/app/.halcyon}"
+if ! (( ${HALCYON_INTERNAL_PATHS:-0} )); then
+	export HALCYON_INTERNAL_PATHS=1
 
-export PATH="${HALCYON_DIR}/ghc/bin:${PATH:-}"
-export PATH="${HALCYON_DIR}/cabal/bin:${PATH}"
-export PATH="${HALCYON_DIR}/sandbox/bin:${PATH}"
-export PATH="${HALCYON_DIR}/app/bin:${PATH}"
-export PATH="${HALCYON_DIR}/slug/bin:${PATH}"
+	export HALCYON_DIR="${HALCYON_DIR:-/app/.halcyon}"
 
-export LIBRARY_PATH="${HALCYON_DIR}/ghc/lib:${LIBRARY_PATH:-}"
-export LIBRARY_PATH="${HALCYON_DIR}/sandbox/.halcyon-sandbox-extra-libs:${LIBRARY_PATH}"
+	export PATH="${HALCYON_TOP_DIR}:${PATH:-}"
+	export PATH="${HALCYON_DIR}/ghc/bin:${PATH}"
+	export PATH="${HALCYON_DIR}/cabal/bin:${PATH}"
+	export PATH="${HALCYON_DIR}/sandbox/bin:${PATH}"
+	export PATH="${HALCYON_DIR}/app/bin:${PATH}"
+	export PATH="${HALCYON_DIR}/slug/bin:${PATH}"
 
-export LD_LIBRARY_PATH="${HALCYON_DIR}/ghc/lib:${LD_LIBRARY_PATH:-}"
-export LIBRARY_PATH="${HALCYON_DIR}/sandbox/.halcyon-sandbox-extra-libs:${LD_LIBRARY_PATH}"
+	export LIBRARY_PATH="${HALCYON_DIR}/ghc/lib:${LIBRARY_PATH:-}"
+	export LIBRARY_PATH="${HALCYON_DIR}/sandbox/.halcyon-sandbox-extra-libs:${LIBRARY_PATH}"
 
-export LANG="${LANG:-en_US.UTF-8}"
+	export LD_LIBRARY_PATH="${HALCYON_DIR}/ghc/lib:${LD_LIBRARY_PATH:-}"
+	export LIBRARY_PATH="${HALCYON_DIR}/sandbox/.halcyon-sandbox-extra-libs:${LD_LIBRARY_PATH}"
+
+	export LANG="${LANG:-en_US.UTF-8}"
+fi
