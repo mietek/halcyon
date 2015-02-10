a7e8e99cffa37139997be199c024a910ba7cf47b
Miëtek Bak <mietek@bak.io>
2015-02-10 15:33:27 +0000

    Use Mark Lentczner’s GHC bindists on OS X

--- src/ghc.sh
+++ src/ghc.sh
@@ -86,6 +86,8 @@ map_ghc_version_to_osx_x86_64_url () {
 
 	# TODO: Improve cross-version compatibility.
 	case "${ghc_version}" in
+	'7.10.1-rc2')	echo 'http://www.ozonehouse.com/mark/platform/ghc-7.10.0.20150123-x86_64-apple-darwin.tar.bz2';;
+	'7.8.4')	echo 'http://www.ozonehouse.com/mark/platform/ghc-7.8.4-x86_64-apple-darwin.tar.bz2';;
 	'7.8.3')	echo 'https://downloads.haskell.org/~ghc/7.8.3/ghc-7.8.3-x86_64-apple-darwin.tar.xz';; # 10.7+
 	'7.8.2')	echo 'https://downloads.haskell.org/~ghc/7.8.2/ghc-7.8.2-x86_64-apple-darwin-mavericks.tar.xz';; # 10.9 only?
 	'7.6.3')	echo 'https://downloads.haskell.org/~ghc/7.6.3/ghc-7.6.3-x86_64-apple-darwin.tar.bz2';; # 10.7+?
