cf34201390ff7039a68802e7f1710e4f12b0bf3d
Miëtek Bak <mietek@bak.io>
2015-03-21 22:14:41 +0000

    Recognise cabal-install 1.22.2.0

--- src/cabal.sh
+++ src/cabal.sh
@@ -10,6 +10,7 @@ map_cabal_version_to_original_url () {
 	'1.20.0.5')	echo 'https://haskell.org/cabal/release/cabal-install-1.20.0.5/cabal-install-1.20.0.5.tar.gz';;
 	'1.20.0.6')	echo 'https://haskell.org/cabal/release/cabal-install-1.20.0.6/cabal-install-1.20.0.6.tar.gz';;
 	'1.22.0.0')	echo 'https://haskell.org/cabal/release/cabal-install-1.22.0.0/cabal-install-1.22.0.0.tar.gz';;
+	'1.22.2.0')	echo 'https://haskell.org/cabal/release/cabal-install-1.22.2.0/cabal-install-1.22.2.0.tar.gz';;
 	*)
 		# NOTE: Bootstrapping cabal-install 1.20.0.4 does not work.
 		# https://www.haskell.org/pipermail/cabal-devel/2014-December/009959.html
