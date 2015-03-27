5a4d25f892719ba6fac9ee78be8b783976b5e696
Miëtek Bak <mietek@bak.io>
2015-03-27 23:51:47 +0000

    WIP

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
-echo '   *** WARNING: Please update your installation instructions' >&2
+echo '   *** WARNING: Please follow up-to-date installation instructions:' >&2
+echo '       https://halcyon.sh/#installation' >&2
 echo >&2
 echo '       To install Halcyon:' >&2
 echo '       $ curl -sL https://github.com/mietek/halcyon/raw/master/bin/setup | bash' >&2
